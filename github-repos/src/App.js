2fc12a515a75c1faba72ad111bba64e2969bc9e9
hirokishingu <hrk.bass.526@gmail.com>
2019-11-15 18:47:04 +0900

    Add previous button..

--- github-repos/src/App.js
+++ github-repos/src/App.js
@@ -32,6 +32,15 @@ class App extends Component {
     event.preventDefault();
   }
 
+  goPrevious(search) {
+    this.setState({
+      first: null,
+      after: null,
+      last: PER_PAGE,
+      before: search.pageInfo.startCursor
+    })
+  }
+
   goNext(search) {
     this.setState({
       first: PER_PAGE,
@@ -80,6 +89,17 @@ class App extends Component {
                       })
                     }
                   </ul>
+
+                  {
+                    search.pageInfo.hasPreviousPage === true ?
+                      <button
+                        onClick={this.goPrevious.bind(this, search)}
+                      >
+                        前へ
+                    </button>
+                      :
+                      null
+                  }
 
                   {
                     search.pageInfo.hasNextPage === true ?
